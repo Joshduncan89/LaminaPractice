f4665d377b3e3aae5afeece64024635d81292b8c
Joshduncan89 <51183094+Joshduncan89@users.noreply.github.com>
2023-04-15 11:55:29 -0400

    clouds to scene

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,15 +1,20 @@
-import { OrbitControls } from '@react-three/drei';
+import { Float, OrbitControls } from '@react-three/drei';
 import { Background } from './Background';
 import { Airplane } from './Airplane';
+import { Cloud } from './Cloud';
 export const Experience = () => {
   return (
     <>
       <OrbitControls />
       <Background />
-      <mesh>
-        <Airplane />
-        <meshNormalMaterial />
-      </mesh>
+
+      <Float floatIntensity={2} speed={2}>
+        <Airplane rotation-y={Math.PI / 2} scale={[0.2, 0.2, 0.2]} />
+      </Float>
+      <Cloud scale={[0.3, 0.3, 0.2]} position={[-2, -1, 3]} />
+      <Cloud scale={[0.1, 0.4, 0.1]} position={[0, 2, 1]} />
+      <Cloud scale={[0.4, 0.1, 0.3]} position={[-2, 1, -3]} />
+      <Cloud scale={[0.2, 0.2, 0.2]} position={[-1, 0, 2]} />
     </>
   );
 };
